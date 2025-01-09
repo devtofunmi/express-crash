16b5f5eaebb0a4dea4dc98c3e1af6123a8bf5468
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-01-10 00:48:08 +0100

    add a delete function

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,7 @@ let posts = [
   { id: 3, title: "post three" },
 ];
 
+
 // Get all posts
 router.get("/", (req, res) => {
   const limit = parseInt(req.query.limit);
@@ -64,6 +65,21 @@ router.put("/:id", (req,res) => {
   post.title=req.body.title;
   res.status(200).json(posts);
 });
+
+//delete post
+router.delete("/:id", (req,res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(400)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+  posts = posts.filter((post) => post.id != id);
+  res.status(200).json(posts);
+});
+
 
 export default router;
 
